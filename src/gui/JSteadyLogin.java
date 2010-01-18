64db33c2da25898c40ccc8a02918759de1411aff
m.hoffmann.hdm <m.hoffmann.hdm@e996a846-fea2-11de-b8ab-b3eb4b1fa784>
2010-01-18 09:42:10 +0000

    Got rid of unused imports.

--- src/gui/JSteadyLogin.java
+++ src/gui/JSteadyLogin.java
@@ -9,7 +9,6 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
