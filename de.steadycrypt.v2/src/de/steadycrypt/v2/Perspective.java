2c765c6d9b467fe61e0bfd3eb93c61ec01c15981
joerg <joerg@echtharrt.de>
2010-10-21 10:33:43 +0000

    added sample view

--- de.steadycrypt.v2/src/de/steadycrypt/v2/Perspective.java
+++ de.steadycrypt.v2/src/de/steadycrypt/v2/Perspective.java
@@ -1,10 +1,15 @@
 package de.steadycrypt.v2;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		
+		layout.addStandaloneView(TableView.ID, true, SWT.RIGHT, 0.5f, layout.getEditorArea());
+		layout.setEditorAreaVisible(false);
+		
 	}
 }
