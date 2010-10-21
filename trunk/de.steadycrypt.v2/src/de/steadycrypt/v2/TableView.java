7a31cdd522cb8b2c7361b53864d077ddf35d4749
joerg@echtharrt.de <joerg@echtharrt.de@e996a846-fea2-11de-b8ab-b3eb4b1fa784>
2010-10-21 10:34:03 +0000

    initial commit of TableView

---
+++ trunk/de.steadycrypt.v2/src/de/steadycrypt/v2/TableView.java
@@ -0,0 +1,26 @@
+package de.steadycrypt.v2;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TableView extends ViewPart {
+	
+	public static String ID = "de.steadycrypt.v2.view.table";
+
+	public TableView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
