5fb369029298b62fd8cee40a68e6009c229ce0ed
joerg <joerg@echtharrt.de>
2010-11-01 12:43:39 +0000

    Added SideBar

--- SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/SteadyPerspective.java
+++ SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/SteadyPerspective.java
@@ -10,13 +10,16 @@ import org.eclipse.swt.SWT;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import de.steadycrypt.v2.views.SideBarView;
 import de.steadycrypt.v2.views.TableView;
 
 public class SteadyPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
 		
-		layout.addStandaloneView(TableView.ID, true, SWT.RIGHT, 0.5f, layout.getEditorArea());
+		layout.addStandaloneView(SideBarView.ID, false, SWT.LEFT, 0.25f, layout.getEditorArea());
+		layout.addStandaloneView(TableView.ID, true, SWT.RIGHT, 0.75f, layout.getEditorArea());
+		
 		layout.setEditorAreaVisible(false);
 		
 	}
