f62bbd8cf8e6229f801d27933752467a025583ff
m.hoffmann.hdm@gmail.com <m.hoffmann.hdm@gmail.com@e996a846-fea2-11de-b8ab-b3eb4b1fa784>
2010-11-03 15:30:07 +0000

    Tree implemented

--- trunk/SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/bob/EncryptedFile.java
+++ trunk/SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/bob/EncryptedFile.java
@@ -1,79 +1,93 @@
+/**
+ * Date: 26.10.2010
+ * SteadyCrypt v2 Project by Joerg Harr and Marvin Hoffmann
+ *
+ */
+
 package de.steadycrypt.v2.bob;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.sql.Date;
 
-import de.steadycrypt.v2.views.model.IDroppedElementVisitor;
+import de.steadycrypt.v2.views.model.IModelVisitor;
 
 public class EncryptedFile extends DroppedElement {
-	protected static List<EncryptedFile> newFiles = buildBookList();
-	protected static int cursor = 0;
+
+	private String type;
+	private Long size;
+	private String file;
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	public EncryptedFile(String title, String authorGivenName, String authorSirName) {
-		super(title, authorGivenName, authorSirName);
-	}
-	
-	
-	
-	
-	public static EncryptedFile newFile() {
-		EncryptedFile newFile = (EncryptedFile)newFiles.get(cursor);
-		cursor = ((cursor + 1) % newFiles.size());
-		return newFile;
-	}
-	
-	
-	protected static List<EncryptedFile> buildBookList() {
-		newFiles = new ArrayList<EncryptedFile>();
-		EncryptedFile[] files = new EncryptedFile[] {
-			new EncryptedFile("Advanced Java: Idioms, Pitfalls, Styles and Programming Tips", "Chris", "Laffra"),
-			new EncryptedFile("Programming Ruby: A Pragmatic Programmer's Guide", "David", "Thomas"),
-			new EncryptedFile("The Pragmatic Programmer", "Andrew", "Hunt"),
-			new EncryptedFile("Java Virtual Machine", "Jon", "Meyer"),
-			new EncryptedFile("Using Netscape IFC", "Arun", "Rao"),
-			new EncryptedFile("Smalltalk-80", "Adele", "Goldberg"),
-			new EncryptedFile("Cold Mountain", "Charles", "Frazier"),
-			new EncryptedFile("Software Development Using Eiffel", "Richard", "Wiener"),
-			new EncryptedFile("Winter's Heart", "Robert", "Jordan"),
-			new EncryptedFile("Ender's Game", "Orson Scott", "Card"),
-			new EncryptedFile("Castle", "David", "Macaulay"),
-			new EncryptedFile("Cranberry Thanksgiving", "Wende", "Devlin"),
-			new EncryptedFile("The Biggest Bear", "Lynd", "Ward"),
-			new EncryptedFile("The Boxcar Children", "Gertrude Chandler", "Warner"),
-			new EncryptedFile("BASIC Fun with Adventure Games", "Susan Drake", "Lipscomb"),
-			new EncryptedFile("Bridge to Terabithia", "Katherine", "Paterson"),
-			new EncryptedFile("One Renegade Cell", "Robert A.", "Weinberg"),
-			new EncryptedFile("Programming Internet Mail", "David", "Wood"),
-			new EncryptedFile("Refactoring", "Martin", "Fowler"),
-			new EncryptedFile("Effective Java", "Joshua", "Bloch"),
-			new EncryptedFile("Cutting-Edge Java Game Programming", "Neil", "Bartlett"),
-			new EncryptedFile("The C Programming Language", "Brian W.", "Kernighan"),
-			new EncryptedFile("The Design and Analysis of Spatial Data Structures", "Hanan", "Samet"),
-			new EncryptedFile("Object-Oriented Programming", "Brad", "Cox"),
-			new EncryptedFile("Python Essential Reference", "David M.", "Beazley"),
-			new EncryptedFile("The Practical SQL Handbook", "Judith S.", "Bowman"),
-			new EncryptedFile("The Design Patterns Smalltalk Companion", "Sherman R.", "Alpert"),
-			new EncryptedFile("Design Patterns", "Erich", "Gamma"),
-			new EncryptedFile("Gig", "John", "Bowe"),
-			new EncryptedFile("You Can't Be Too Careful", "David Pryce", "Jones"),
-			new EncryptedFile("Go for Beginners", "Kaoru", "Iwamoto"),
-			new EncryptedFile("How to Read a EncryptedFile", "Mortimer J.", "Adler"),
-			new EncryptedFile("The Message", "Eugene H.", "Peterson"),
-			new EncryptedFile("Beyond Bumper Sticker Ethics", "Steve", "Wilkens"),
-			new EncryptedFile("Life Together", "Dietrich", "Bonhoeffer"),
-			new EncryptedFile("Java 2 Exam Cram", "William", "Brogden")
-		};
-		
-		for (int i = 0; i < files.length; i++) {
-			newFiles.add(files[i]);
-			
-		}
-		return newFiles;
-	}
-	/*
-	 * @see DroppedElement#accept(ModelVisitorI, Object)
+	/**
+	 * Used when a new file was dropped.
+	 * @param newFile
 	 */
-	public void accept(IDroppedElementVisitor visitor, Object passAlongArgument) {
+	public EncryptedFile(File newFile)
+	{
+		super(newFile.getName(), new Date(System.currentTimeMillis()), newFile.getPath());
+		this.type = defineFileType();
+		this.size = newFile.length();
+		this.file = System.nanoTime()+".sc";
+	}
+
+	/**
+	 * Used when the content table is being read.
+	 * @param name
+	 * @param type
+	 * @param size
+	 * @param date
+	 * @param path
+	 * @param file
+	 */
+	public EncryptedFile(String name, String type, long size, Date date, String path, String file)
+	{
+		super(name, date, path);
+		this.type = type;
+		this.size = size;
+		this.file = file;
+	}
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Long getSize() {
+		return size;
+	}
+	
+	public void setSize(Long size) {
+		this.size = size;
+	}
+	
+	public String getFile()
+	{
+		return file;
+	}
+	
+	public void setFile(String file) {
+		this.file = file;
+	}
+	
+	public String defineFileType()
+	{
+		String ext = (this.name.lastIndexOf(".")==-1)?"":this.name.substring(this.name.lastIndexOf(".")+1,this.name.length());
+
+		String fileType = ext+"-File";
+		
+		return fileType;
+	}
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public void accept(IModelVisitor visitor, Object passAlongArgument)
+	{
 		visitor.visitFile(this, passAlongArgument);
 	}
 
