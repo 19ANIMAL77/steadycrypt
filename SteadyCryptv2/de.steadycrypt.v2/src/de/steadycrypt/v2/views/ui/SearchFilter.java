9b5ea0c9d7bc1f353a076166599e31f323f38a61
joerg <joerg@echtharrt.de>
2010-11-08 21:08:11 +0000

    initial commit

--- SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/views/ui/SearchFilter.java
+++ SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/views/ui/SearchFilter.java
@@ -5,17 +5,65 @@ package de.steadycrypt.v2.views.ui;
  *
  */
 
-
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
+import de.steadycrypt.v2.bob.DroppedElement;
+import de.steadycrypt.v2.bob.dob.EncryptedFileDob;
 import de.steadycrypt.v2.bob.dob.EncryptedFolderDob;
+import de.steadycrypt.v2.views.SideBarView;
 
 public class SearchFilter extends ViewerFilter {
 
-	public boolean select(Viewer viewer, Object parentElement, Object element)
+	public boolean select(Viewer viewer, Object parentElement, Object element){
+		
+		if(element instanceof EncryptedFileDob && ((EncryptedFileDob)element).getName().toUpperCase().contains(SideBarView.searchString.toUpperCase())){
+			return true;
+		} else if (element instanceof EncryptedFolderDob){
+			return find((DroppedElement) element, SideBarView.searchString);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param element
+	 * @param searchString
+	 * @return boolean
+	 */
+	private boolean find(DroppedElement element, String searchString)
 	{
-		return true;
-//		return parentElement instanceof EncryptedFolderDob && ((EncryptedFolderDob)parentElement).size() >= 3;
+		boolean found = false;
+		String contents = element.getName().toUpperCase();
+		
+		if(element instanceof EncryptedFileDob)
+		{
+			if ((contents.contains(searchString.toUpperCase()))) 
+			{
+				return found = true;
+			}			
+		}
+		else if(element instanceof EncryptedFolderDob)
+		{
+			EncryptedFolderDob folderToDecrypt = (EncryptedFolderDob)element;
+						
+			for(EncryptedFolderDob nextFolderToDecrypt : folderToDecrypt.getFolders())
+			{
+				found = found == true ? true : find(nextFolderToDecrypt, searchString);
+			}
+			
+			for(EncryptedFileDob nextFileToDecrypt : folderToDecrypt.getFiles())
+			{
+				found =  found == true ? true : find(nextFileToDecrypt, searchString);
+			}
+			
+			if ((contents.contains(searchString.toUpperCase()))) 
+			{
+				return found = true;
+			}	
+		}
+		
+		return found;
 	}
 }
