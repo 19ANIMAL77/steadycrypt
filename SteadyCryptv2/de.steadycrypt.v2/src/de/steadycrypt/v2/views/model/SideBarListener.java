7449b499784753cf6ae66766b3b2463929d0b331
joerg <joerg@echtharrt.de>
2010-11-08 21:08:29 +0000

    initial commit

--- SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/views/model/SideBarListener.java
+++ SteadyCryptv2/de.steadycrypt.v2/src/de/steadycrypt/v2/views/model/SideBarListener.java
@@ -17,6 +17,6 @@ public interface SideBarListener extends EventListener {
 	/**
 	 * This Event is triggered by my Button experiment
 	 */
-	public void findItem(String searchString);
+	public void doSearch();
 
 }
